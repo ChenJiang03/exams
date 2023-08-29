d0566dd5c6adc814b1345630fa66dd1acf2dae12
root <961834697@qq.com>
2023-08-29 15:04:32 +0800

    配置

---
+++ src/main/java/com/wanmait/exam/manageController/ManageTeacherController.java
@@ -0,0 +1,18 @@
+package com.wanmait.exam.manageController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * 教师表 前端控制器
+ * </p>
+ *
+ * @author wanmait
+ * @since 2023-08-29
+ */
+@Controller
+@RequestMapping("/teacher")
+public class ManageTeacherController {
+
+}

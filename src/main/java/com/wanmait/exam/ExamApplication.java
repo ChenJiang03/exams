43ad6fd45125a6c95b0828521ff2bba163f9f5e9
root <961834697@qq.com>
2023-08-29 13:38:02 +0800

    1

--- src/main/java/com/wanmait/exam/ExamApplication.java
+++ src/main/java/com/wanmait/exam/ExamApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExamApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ExamApplication.class, args);
+        System.out.println(11);
     }
 
 }

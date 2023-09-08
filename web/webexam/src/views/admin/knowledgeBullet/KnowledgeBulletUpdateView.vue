3cbcbadecd458f2c1721ce9111bf42c095577e19
Nancy <nancyswift1212@outlook.com>
2023-09-08 16:36:32 -0400

    添加了修改知识点功能

--- web/webexam/src/views/admin/knowledgeBullet/KnowledgeBulletUpdateView.vue
+++ web/webexam/src/views/admin/knowledgeBullet/KnowledgeBulletUpdateView.vue
@@ -1,10 +1,91 @@
-<script setup>
-
-</script>
-
 <template>
+  <div>
+    <el-form :model="knowledgeBullet" :rules="rules" ref="knowledgeBulletForm" label-width="100px" class="demo-ruleForm">
 
+      <el-form-item label="知识点" prop="knowledgePoint">
+        <el-input v-model="knowledgeBullet.knowledgePoint"></el-input>
+      </el-form-item>
+      <el-form-item label="对应科目" prop="subject">
+        <el-select v-model="knowledgeBullet.subjectId">
+          <el-option :label="subject.title" :value="subject.id"
+                     v-for="(subject,index) in subjects"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">修改</el-button>
+        <el-button @click="resetForm('knowledgeBulletForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "QuestionAddView",
+  data() {
+    return {
+      subjects: [],
+      knowledgeBullet: {
+        id:'',
+        knowledgePoint: '',
+        subjectId: '',
+      },
+      rules: {
+        knowledgePoint: [
+          {required: true, message: '请输入知识点名称', trigger: 'blur'},
+          {min: 2, max: 30, message: '长度在 2 到 30 个字符', trigger: 'blur'}
+        ]
+      }
+    };
+  },
+  //将id拿到被修改的对象当中
+  mounted() {
+
+  },
+  created() {
+    this.knowledgeBullet.id = this.$route.query.id;
+    this.getKnowledgeBullet();
+    this.getSubjects();
+  },
+  methods: {
+    getSubjects() {
+      this.$axios.get("manage/subject/list")
+          .then(res => {
+            this.subjects = res.data.data;
+            //将添加知识点时候的科目默认值设置为第一个科目
+            this.knowledgeBullet.subjectId = this.subjects[0].id;
+          });
+    },
+    getKnowledgeBullet() {
+      this.$axios.get("manage/knowledgeBullet/findById", {params: {id: this.knowledgeBullet.id}})
+          .then(res => {
+            this.knowledgeBullet = res.data.data;
+            console.log("知识点JSON对象：" + this.knowledgeBullet);
+          });
+    },
+    submitForm() {
+      //this.$refs["questionForm"].validate((valid) => {
+      //validate()方法表示对表单进行一遍验证，匿名函数的参数valid为验证的结果，如果所有验证都通过true，有一个不通过false
+      this.$refs.knowledgeBulletForm.validate((valid) => {
+        if (valid) {
+          //axios提交form表单数据
+          //this.$axios.post("http://localhost:8080/manage/question/insert",this.question,{params:{}})
+          this.$axios.post("manage/knowledgeBullet/update", this.knowledgeBullet)
+              .then(res => {
+                this.$router.push("list");
+              });
+        } else {
+          console.log("验证失败");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
 
 <style scoped>
 
